65956d89614cc8437f62088cc0b76b1ba7f9e2db
Jirka <you@example.com>
2023-10-24 15:28:13 +0200

    osu

---
+++ res/js/main.js
@@ -0,0 +1,36 @@
+const point = document.getElementById("point")
+const startgame = document.getElementById("startgame")
+
+let pointInterval;
+
+startgame.onclick = ( ) => {
+    startgame.style.display = "none"
+startPointInterval();
+
+}
+const movePoint = (element, x, y) => {
+element.style.top = `${y}px`;
+element.style.left = `${x}px`;
+
+}
+const startPointInterval = () => {
+    clearInterval(pointInterval)
+    setPointClick(point);
+    pointInterval = setInterval(()=>{
+        setSize(point, getRandomNumber(40,70));
+        movePoint(point, getRandomNumber(100,600), getRandomNumber(100,600));
+    },700);
+}
+
+const getRandomNumber = (minimum, maximum) => Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+const setPointClick = (element) => {
+    element.onclick = () => {
+        element.innerText++;
+
+    }
+}
+const setSize = (element, size) => {
+element.style.widht = `${size}px`;
+element.style.height = `${size}px`;
+}
+
