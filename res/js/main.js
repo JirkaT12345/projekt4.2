d4927f5b2afac3153c02004833e2c3e5de2a03ad
Jirka <you@example.com>
2023-10-31 15:08:04 +0100

    osu

--- res/js/main.js
+++ res/js/main.js
@@ -1,11 +1,13 @@
 const point = document.getElementById("point")
 const startgame = document.getElementById("startgame")
+const timeInfo = document.getElementById("timeInfo")
 
 let pointInterval;
 
 startgame.onclick = () => {
     startgame.style.display = "none"
     startPointInterval();
+
 
 };
 const movePoint = (element, x, y) => {
@@ -25,8 +27,18 @@ const startPointInterval = () => {
 const getRandomNumber = (minimum, maximum) =>
     Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
+
+let timeStart = 0;
 const setPointClick = (element) => {
     element.onclick = () => {
+if (timeStart ==0) {
+    timeStart = performance.now();
+}else {
+    let timeEnd = performance.now();
+    let result = timeEnd - timeStart;
+    timeInfo.innerText = `Time: ${result}ms`
+    timeStart = performance.now();
+}
         element.innerText++;
         movePoint(point, getRandomNumber(parseFloat(point.style.width), window.innerWidth - parseFloat(point.style.width)), getRandomNumber(parseFloat(point.style.height), window.innerHeight - parseFloat(point.style.height)));
     };
