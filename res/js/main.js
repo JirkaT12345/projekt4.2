77fdcb5100525c71ba961e1496be49318d3ecef9
Jirka <you@example.com>
2023-10-31 14:47:00 +0100

    osu

--- res/js/main.js
+++ res/js/main.js
@@ -3,34 +3,36 @@ const startgame = document.getElementById("startgame")
 
 let pointInterval;
 
-startgame.onclick = ( ) => {
+startgame.onclick = () => {
     startgame.style.display = "none"
-startPointInterval();
+    startPointInterval();
 
-}
+};
 const movePoint = (element, x, y) => {
-element.style.top = `${y}px`;
-element.style.left = `${x}px`;
+    element.style.top = `${y}px`;
+    element.style.left = `${x}px`;
 
-}
+};
 const startPointInterval = () => {
     clearInterval(pointInterval)
     setPointClick(point);
-    pointInterval = setInterval(()=>{
-        setSize(point, getRandomNumber(50,70));
-        movePoint(point, getRandomNumber(1,600), getRandomNumber(1,600));
-    },700);
-}
+    pointInterval = setInterval(() => {
+        setSize(point, getRandomNumber(40, 70));
+        movePoint(point, getRandomNumber(parseFloat(point.style.width), window.innerWidth - parseFloat(point.style.width)), getRandomNumber(parseFloat(point.style.height), window.innerHeight - parseFloat(point.style.height)));
+    }, 600);
+};
 
-const getRandomNumber = (minimum, maximum) => Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+const getRandomNumber = (minimum, maximum) =>
+    Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
 const setPointClick = (element) => {
     element.onclick = () => {
         element.innerText++;
-
-    }
-}
+        movePoint(point, getRandomNumber(parseFloat(point.style.width), window.innerWidth - parseFloat(point.style.width)), getRandomNumber(parseFloat(point.style.height), window.innerHeight - parseFloat(point.style.height)));
+    };
+};
 const setSize = (element, size) => {
-element.style.width = `${size}px`;
-element.style.height = `${size}px`;
-}
+    element.style.width = `${size}px`;
+    element.style.height = `${size}px`;
+};
 
